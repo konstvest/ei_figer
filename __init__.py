b780ab26014f02d3c665e392af39c6883b76c995
konstvest <30634453+konstvest@users.noreply.github.com>
2022-08-17 01:13:08 +0300

    up version to 5.1

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@
 bl_info = {
     'name': 'EI figer',
     'author': 'konstvest',
-    'version': (5, 0),
+    'version': (5, 1),
     'blender': (3, 0, 0),
     'location': '',
     'description': 'Addon for import/export models from Evil Islands to Blender',
